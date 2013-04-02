bc28d52f50aa3e1c1de9871eeae56be5d187c54c
Eljazovic Samir <samir@atlantbh.com>
2013-04-02 13:04:22 +0200

    Removed UUID tools

--- app/controllers/jmbg_controller.rb
+++ app/controllers/jmbg_controller.rb
@@ -1,16 +1,19 @@
-require "uuidtools"
-
 class JmbgController < ApplicationController
   MAX = 999999999999
-
 
   def index
   end
 
   def new
-    @jmbg = rand(MAX).to_s.center(13, rand(9).to_s)
+    @jmbg = get_jmbg()
+    
     flash[:msg] = { :type => 'info', :text => "JMBG kreiran: #{@jmbg}"}
 
-   redirect_to root_path()
+    redirect_to root_path()
+  end
+
+  private
+  def get_jmbg
+    rand(MAX).to_s.center(13, rand(9).to_s)
   end
 end
