98014bd02ea4e1d8522270ec0ebf03575a79e3cb
Eljazovic Samir <samir@atlantbh.com>
2013-04-02 11:09:57 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maticnibroj::Application.config.secret_token = 'fdaf86ee17734a0cc5dce06e65c27a7f19dc91387e58eb6741c95d7758e6f3dd89080f9f479252743814ffdfc13af40d59de3230c48845a8943a8a857a3eab7a'
