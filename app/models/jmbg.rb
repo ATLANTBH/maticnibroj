db633a0a38c3f78cc6b73670b864b7f723b72896
Eljazovic Samir <samir@atlantbh.com>
2013-04-05 22:50:06 +0200

    Added note

--- app/models/jmbg.rb
+++ app/models/jmbg.rb
@@ -3,6 +3,8 @@ require 'verhoeff'
 
 class Jmbg < ActiveRecord::Base
   def generate
-    self.jmbg_number = Verhoeff.checksum_of(Luhnacy.generate(12))
+    begin
+      self.jmbg_number = Verhoeff.checksum_of(Luhnacy.generate(12)).to_s
+    end while self.jmbg_number.size != 13
   end
 end
